e4c985d9f4faa9dc01618fb1880ae73b828f9842
Jason Scherer <schereja@gmail.com>
2015-10-09 15:10:37 -0500

    Initial Revision

---
+++ helpers/encrypter.js
@@ -0,0 +1,26 @@
+var crypto     = require('crypto');
+
+function encrypt(key, data) {
+    var cipher = crypto.createCipher('aes256', key);
+    var crypted = cipher.update(data, 'utf-8', 'hex');
+    crypted += cipher.final('hex');
+
+    return crypted;
+}
+
+function decrypt(key, data) {
+    var decipher = crypto.createDecipher('aes256', key);
+    var decrypted = decipher.update(data, 'hex', 'utf-8');
+    decrypted += decipher.final('utf-8');
+
+    return decrypted;
+}
+
+var key = new Buffer('85CE6CCF67FBBAA8BB13479C3A6E084D', 'hex');
+var test = {
+    "host" : "test",
+    "user" : "edison",
+    "password" : encrypt(key, 'test'),
+    "database" : "Edison"
+}
+console.log(test);
